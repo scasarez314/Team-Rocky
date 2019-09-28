528ef5a520ff95f14485501765dc70ab993125ae
RobS <51037713+RobS30@users.noreply.github.com>
2019-09-28 12:19:19 -0500

    Added ajax call to pull form data and setted up API routes to handle incoming calls. Confirmed endpoints working.  Also cleaned up file tree.

---
+++ public/js/form.js
@@ -0,0 +1,67 @@
+// Capture the form inputs
+$("#formSubmit").on("click", function(event) {
+  event.preventDefault();
+
+  // Form validation to make sure form is filled out before POST via AJAX
+  function validateForm() {
+    var isValid = true;
+    $(".form-control").each(function() {
+      if ($(this).val() === "") {
+        isValid = false;
+      }
+    });
+    $(".chosen-select").each(function() {
+      if ($(this).val() === "") {
+        isValid = false;
+      }
+    });
+    return isValid;
+  }
+
+  // If all required fields are filled
+  if (validateForm()) {
+    // Create an object for the user"s data
+    var userFormData = {
+      name: $("#formNameInput").val().trim(),
+      telephone: $("#formTelephoneInput").val().trim(),
+      email: $("#formEmailInput").val().trim(),
+      date:$("#formDateInput").val().trim(),
+      location:$("#locationFormInput").val().trim(),
+      description: $("#formDescriptionInput").val().trim(),
+  };
+    // AJAX post the data to the events API
+    $.ajax("/api/events", {
+      type: "POST",
+      data: userFormData
+    }).then(function(data) {
+      // this will reload our page to get the updated list of events  (Do we need this since our form is on a different page?)
+      location.reload();
+    });
+  } else {
+    alert("Your event could not be created.  Please fill out the form completely.");
+  }
+});
+
+// This function grabs posts from the database and updates the view
+function getEvents() {
+$.get("/api/events", function(data) {
+posts = data;
+if (!posts || !posts.length) {
+  displayEmpty(author);
+}
+else {
+  initializeRows();
+}
+});
+}
+
+// This function does an API call to delete posts
+//  function deletePost(id) {
+//   $.ajax({
+//     method: "DELETE",
+//     url: "/api/posts/" + id
+//   })
+//     .then(function() {
+//       getPosts(postCategorySelect.val());
+//     });
+// }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,26 +1,26 @@
 // Dependencies
 var db = require("../models");
-var path = require("path")
+var path = require("path");
 
-module.exports = function (app) {
+module.exports = function(app) {
 
   // Load home page on inital / search
-  app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"))
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/home.html"))
   });
 
   // Load home page for the navigation bar
-  app.get("/home", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"))
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/home.html"))
   });
 
   // Load the form page
-  app.get("/form", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/form.html"))
+  app.get("/form", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/form.html"))
   });
 
   // Leftovers from boiler plate code
-  app.get("/example/:id", function (req, res) {
+  app.get("/example/:id", function req, res) {
     db.Form.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
@@ -29,7 +29,7 @@ module.exports = function (app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
+  // app.get("*", function (req, res) {
+  //   res.render("404");
+  // });
 };
