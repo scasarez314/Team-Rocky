387008c60774f3238b2a7dac7f715c397ed469fa
Sierra Casarez <scasarez314@gmail.com>
2019-09-28 14:15:55 -0500

    updating the api routes and the mysql

--- models/form.js
+++ models/form.js
@@ -2,9 +2,10 @@ module.exports = function (sequelize, DataTypes) {
   var Form = sequelize.define("Form", {
 
     name: DataTypes.STRING,
+    telephone: DataTypes.STRING,
     email: DataTypes.STRING,
     date: DataTypes.DATE,
-    zipcode: DataTypes.INTEGER,
+    Location: DataTypes.STRING,
     description: DataTypes.STRING
 
 
