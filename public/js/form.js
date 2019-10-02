0dc107b9358fa551e578fedfdc0a6d5f4c04f8e0
Sierra Casarez <scasarez314@gmail.com>
2019-10-01 19:31:19 -0500

    Creating the Ajax calls

--- public/js/form.js
+++ public/js/form.js
@@ -1,16 +1,16 @@
 // Capture the form inputs
-$("#formSubmit").on("click", function(event) {
+$("#formSubmit").on("click", function (event) {
   event.preventDefault();
 
   // Form validation to make sure form is filled out before POST via AJAX
   function validateForm() {
     var isValid = true;
-    $(".form-control").each(function() {
+    $(".form-control").each(function () {
       if ($(this).val() === "") {
         isValid = false;
       }
     });
-    $(".chosen-select").each(function() {
+    $(".chosen-select").each(function () {
       if ($(this).val() === "") {
         isValid = false;
       }
@@ -25,43 +25,64 @@ $("#formSubmit").on("click", function(event) {
       name: $("#formNameInput").val().trim(),
       telephone: $("#formTelephoneInput").val().trim(),
       email: $("#formEmailInput").val().trim(),
-      date:$("#formDateInput").val().trim(),
-      location:$("#locationFormInput").val().trim(),
+      date: $("#formDateInput").val().trim(),
+      location: $("#locationFormInput").val().trim(),
       description: $("#formDescriptionInput").val().trim(),
-  };
+    };
     // AJAX post the data to the events API
     $.ajax("/api/events", {
       type: "POST",
       data: userFormData
-    }).then(function(data) {
+    }).then(function (data) {
       // this will reload our page to get the updated list of events  (Do we need this since our form is on a different page?)
       location.reload();
+
     });
   } else {
     alert("Your event could not be created.  Please fill out the form completely.");
   }
 });
 
+getEvents();
+
 // This function grabs posts from the database and updates the view
 function getEvents() {
-$.get("/api/events", function(data) {
-posts = data;
-if (!posts || !posts.length) {
-  displayEmpty(author);
-}
-else {
-  initializeRows();
-}
-});
+  $.get("/api/events", function (data) {
+
+    for (let i = 0; i < data.length; i++) {
+      const element = data[i];
+      console.log(data[i])
+      // $("#event-table > tbody").append(
+      //   $("<tr>").append(
+      //     $("<td>").v(data[i].name),
+      //     $("<td>").text(data[i].telephone),
+      //     $("<td>").text(data[i].email),
+      //     $("<td>").text(data[i].date),
+      //     $("<td>").text(data[i].location),
+      //     $("<td>").text(data[i].description)
+      //   )
+
+    }
+
+  });
 }
 
-// This function does an API call to delete posts
-//  function deletePost(id) {
-//   $.ajax({
-//     method: "DELETE",
-//     url: "/api/posts/" + id
-//   })
-//     .then(function() {
-//       getPosts(postCategorySelect.val());
-//     });
-// }
+//Edit the event
+
+
+// This function does an API call to delete posts **need to create this button**
+
+// $(".delete-event").on("click", function(event) {
+//   var id = $(this).data("id");
+
+//   // Send the DELETE request.
+//   $.ajax("/api/events/" + id, {
+//     type: "DELETE"
+//   }).then(
+//     function(data) {
+//       console.log("deleted event", id);
+//       // Reload the page to get the updated list
+//       location.reload();
+//     }
+//   );
+// });
