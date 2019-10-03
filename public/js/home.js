4d7c297bc8cd856bf02de654132acd2fa78e0c7e
RobS <51037713+RobS30@users.noreply.github.com>
2019-10-03 12:24:39 -0500

    added base code for gmaps into html and api boilerplate to js file

--- public/js/home.js
+++ public/js/home.js
@@ -1,16 +1,33 @@
-// var formFields = [
+// ajax get request to get zip code
+// loop through database
 
-//     $.ajax({
-//         method: 'POST',
-//         url: '/api/form',
-//         data: {
-//             name: 'Justin',
-//             age: 'unknown'
-//         }
-//     })
+// goecoding service to get lat and long
+var lat = '';
+var lng = '';
+var address = {zipcode} or {city and state};
+geocoder.geocode( { 'address': address}, function(results, status) {
+  if (status == google.maps.GeocoderStatus.OK) {
+     lat = results[0].geometry.location.lat();
+     lng = results[0].geometry.location.lng();
+    });
+  } else {
+    alert("Geocode was not successful for the following reason: " + status);
+  }
+});
+alert('Latitude: ' + lat + ' Logitude: ' + lng);
 
+// push the markers to the map
+function initMap() {
+  var myLatLng = { lat: 32.7767, lng: -96.797 };
 
-// ];
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: myLatLng
+  });
 
-
-// module.exports = formFields
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: "Hello World!"
+  });
+}
